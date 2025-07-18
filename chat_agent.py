6938ffc09e2732b69f30b1422a26626ab25bd075
LeoXLiu-DS <LeoXLiu-DS>
2025-07-18 22:44:57 +0000

    Updated the agent script

--- chat_agent.py
+++ chat_agent.py
@@ -1,16 +1,13 @@
-from typing import Any, Generator, Optional, Sequence, Union
-from langchain.chat_models import init_chat_model
-from langchain.chat_models.base import BaseChatModel
+from typing import Any, Generator, Optional, TypedDict, Annotated
 from langchain_core.tools import tool
-from typing import TypedDict, Annotated
-from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage
+from langchain_core.messages import AnyMessage, SystemMessage
 from langgraph.graph import StateGraph, START, END
-from langgraph.graph.state import CompiledStateGraph
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
 from langchain_community.chat_models.databricks import ChatDatabricks
 from langgraph.checkpoint.memory import InMemorySaver
-from mlflow.langchain.chat_agent_langgraph import ChatAgentState, ChatAgentToolNode
+from langgraph.graph.state import CompiledStateGraph
+from mlflow.langchain.chat_agent_langgraph import parse_message
 from databricks.vector_search.client import VectorSearchClient
 from mlflow.pyfunc import ChatAgent
 from mlflow.types.agent import (
@@ -19,9 +16,6 @@ from mlflow.types.agent import (
     ChatAgentResponse,
     ChatContext,
 )
-from mlflow.langchain.chat_agent_langgraph import parse_message
-from langgraph.graph.state import CompiledStateGraph
-
 import mlflow
 
 mlflow.langchain.autolog()
@@ -31,18 +25,12 @@ mlflow.set_registry_uri("databricks-uc")
 # Configure
 CATALOG = "workspace"
 SCHEMA = "genai_demo"
-
-# Anomaly Detection Model
 MODEL_NAME = "isolation_forest_pm_model"
 MODEL_NAME_FULL = f"{CATALOG}.{SCHEMA}.{MODEL_NAME  }"
 MODEL_VERSION = 1
 MODEL_URI = f'models:/{MODEL_NAME_FULL}/{MODEL_VERSION}'
-
-# Vector Index
 INDEX_NAME = "maintenance_docs_index"
 INDEX_NAME_FULL = f"{CATALOG}.{SCHEMA}.{INDEX_NAME}"
-
-# LLM
 LLM_MODEL = "gpt-41"
 TEMPERATURE = 0.1
 # LLM_MODEL = "databricks-llama-4-maverick"
@@ -50,12 +38,8 @@ TEMPERATURE = 0.1
 
 # Load resources: model, retriever, LLM
 ad_model = mlflow.sklearn.load_model(MODEL_URI)
-
 vsc = VectorSearchClient()
 index = vsc.get_index(index_name=INDEX_NAME_FULL)
-
-# ws = WorkspaceClient()
-# chat_client = ws.serving_endpoints.get_open_ai_client()
 llm = ChatDatabricks(
     target_uri="databricks",
     endpoint=LLM_MODEL,
